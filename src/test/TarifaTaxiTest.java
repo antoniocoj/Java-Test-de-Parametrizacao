af96f32a5ab837c5c4caff103b7dbb44009596a1
antoniocoj <_antonio_@hotmail.com>
2018-09-21 10:38:41 -0300

    Triangulação dos testes por meio de teste parametrizado

--- src/test/TarifaTaxiTest.java
+++ src/test/TarifaTaxiTest.java
@@ -2,27 +2,45 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import app.CalcularTaxiTarifa;
 
+@RunWith(Parameterized.class)
 public class TarifaTaxiTest {
 
-	@Test
-	public void tarifaTaxiTest() {
-		assertEquals(290.24f,CalcularTaxiTarifa.calculoTarifa("Bandeira 1", 100, 0), 0.00f);
-		
+	String bandeirada;
+	int km;
+	int hora;
+	float valor;
+	
+	public TarifaTaxiTest (String bandeirada, int km, int hora, float valor) {
+		this.bandeirada = bandeirada;
+		this.km = km;
+		this.hora = hora;
+		this.valor = valor;
 	}
 	
-	@Test
-	public void tarifaTaxiOutroValorTest() {
-		assertEquals(321.96f,CalcularTaxiTarifa.calculoTarifa("Bandeira 1", 100, 1), 0.00f);
-		
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+			{"Bandeira 1",100,0,290.24f},
+			{"Bandeira 2",100,0,371.24f},
+			{"Bandeira 1",50,1,179.46f},
+			{"Bandeira 2",50,1,219.96f},
+			{"Bandeira 1",30,1,122.46f},
+			{"Bandeira 2",30,2,178.48f}
+		});
 	}
-	
 	@Test
-	public void tarifaTaxiBandeira2Test() {
-		assertEquals(178.48f,CalcularTaxiTarifa.calculoTarifa("Bandeira 2", 30, 2), 0.00f);
-		
+	public void testCalculoTaxiDF(){
+		assertEquals(valor, CalcularTaxiTarifa.calculoTarifa(bandeirada, km, hora), 0.01f);
 	}
+
 }
