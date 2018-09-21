71d4f9dbb57287b6c057be4100bc4146c0891e42
antoniocoj <_antonio_@hotmail.com>
2018-09-21 10:21:37 -0300

    Primeiro Teste - Barra Vermelha

---
+++ src/test/TarifaTaxiTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TarifaTaxiTest {
+
+	@Test
+	public void tarifaTaxiTest() {
+		assertEquals(290.24f,CalcularTaxiTarifa.calculoTarifa("Bandeira 1", 100, 0), 0.00f);
+		
+	}
+
+}
