1f895ea485d66990e170c9431ca6574a642ebe6f
antoniocoj <_antonio_@hotmail.com>
2018-09-21 10:32:33 -0300

    Segundo teste - Passando criação do corpo do método

--- src/app/CalcularTaxiTarifa.java
+++ src/app/CalcularTaxiTarifa.java
@@ -3,7 +3,21 @@ package app;
 public class CalcularTaxiTarifa {
 
 	public static float calculoTarifa(String bandeirada, int km, int hora) {
-		
-		return 290.24f;
+		float valor = 0.00f;
+
+		if (km != 0) {
+			if (bandeirada != null){
+				if (bandeirada == "Bandeira 1") {
+					valor = 5.24f + (km * 2.85f) + (31.72f * hora);
+				} else if (bandeirada == "Bandeira 2") {
+					valor = 5.24f + (km * 3.66f) + (31.72f * hora);
+				}
+			} else {
+
+			}
+		} else {
+
+		}
+		return valor;
 	}
 }
